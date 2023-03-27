25059d014848fb570e98775609d6d8b95f6f4a08
Lucas Marques <lucasmarques630@gmail.com>
2023-03-27 20:29:03 -0300

    Primeiro commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route element={<App />}>
-        <Route path="/" element={Home}/>
-        <Route path="movie/:id" element={Movie}/>
-        <Route path="search" element={Search}/>
-
-      </Route>
-    </Routes>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
